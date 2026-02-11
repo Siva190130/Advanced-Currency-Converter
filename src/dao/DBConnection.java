3abe9462939188c4003b805845494acfc6c68bc3
Sivasankar <Sivasankar.Kambhampalli@partner.bmw.de>
2026-02-11 13:06:32 +0530

    feat: dockarized the Application.

--- src/dao/DBConnection.java
+++ src/dao/DBConnection.java
@@ -2,20 +2,44 @@ package dao;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
 
 public class DBConnection {
-    private static final String url = "jdbc:mysql://localhost:3306/currencydb";
-    private static final String user = "root";
-    private static final String password = "root"; 
 
-    public static Connection getConnection() throws SQLException {
+    private static final String URL =
+        "jdbc:mysql://mysql:3306/currencydb?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
+
+    private static final String USER = "root";
+    private static final String PASSWORD = "root";
+
+    static {
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver"); 
-        } catch (ClassNotFoundException e) {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (Exception e) {
+            System.err.println("MySQL Driver not found!");
             e.printStackTrace();
         }
-        return DriverManager.getConnection(url, user, password);
+    }
+
+    public static Connection getConnection() {
+
+        int retries = 10;
+
+        while (retries-- > 0) {
+            try {
+                System.out.println("Trying to connect to MySQL...");
+                return DriverManager.getConnection(URL, USER, PASSWORD);
+
+            } catch (Exception e) {
+                System.out.println("MySQL not ready, retrying in 3 seconds...");
+                try {
+                    Thread.sleep(3000);
+                } catch (InterruptedException ie) {
+                    ie.printStackTrace();
+                }
+            }
+        }
+
+        System.err.println("Unable to connect to MySQL after multiple attempts.");
+        return null;   // return null instead of throwing exception
     }
 }
-
